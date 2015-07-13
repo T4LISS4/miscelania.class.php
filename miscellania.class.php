dc9db8ae0d1fb64bf3795c1bdb9db8e572a6d2ce
T4LISS4 <talissaalves123@hotmail.com>
2015-07-12 21:41:12 -0300

    Calcula Milhas
    Calcular Milhas e Metros e Vice Versa

--- miscellania.class.php
+++ miscellania.class.php
@@ -1,5 +1,26 @@
 <?php
 class Miscellania {
 
+public $em;
+public $valor;
+
+function mikmConvert($em, $valor) {
+    if($em=="k") {
+        
+        $resultado = $valor / 1.609344;
+
+        return "$resultado";
+    }
+    elseif($em=="m") {
+        
+        $resultado = $valor * 1.609344;
+
+        return "$resultado";
+    }
+    else {
+        return false;
+    }
+}  
+
 }
 ?>
